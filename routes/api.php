d1e5c996724d41f4164229c47bac96f625a94221
GioevArsen <gioev.arsen15@list.ru>
2026-03-27 20:03:30 +0300

    testing autodeploy upon merging branches fix

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::prefix('v1')->group(function () {
 
     Route::get('/health-check', function() {
         return response()->json([
-            'status' => 'active',
+            'status' => 'ok',
             'service' => 'betterlk-back',
             'version' => 'v1',
             'timestamp' => now(),
